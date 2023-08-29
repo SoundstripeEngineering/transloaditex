d0e5a287064752f9e0a13c6614e7b3de3e23ed94
Larry J Rutledge <1201306+larryrutledge@users.noreply.github.com>
2023-08-28 20:46:37 -0500

    Remove auth keys

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,7 @@
 import Config
 
 config :transloaditex,
-  auth_key: "1526fdc376af415795be46f6c2b979a6",
-  auth_secret: "a2dd4f8707164d2bb0738cee70056c6ed15b4c19",
-  # auth_key: System.get_env("TRANSLOADIT_AUTH_KEY"),
-  # auth_secret: System.get_env("TRANSLOADIT_AUTH_SECRET"),
+  auth_key: System.get_env("TRANSLOADIT_AUTH_KEY"),
+  auth_secret: System.get_env("TRANSLOADIT_AUTH_SECRET"),
   max_retries: String.to_integer(System.get_env("TRANSLOADIT_MAX_RETRIES", "10")),
   duration: String.to_integer(System.get_env("TRANSLOADIT_DURATION", "300"))
